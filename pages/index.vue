ca8187c7818e5f5328639ec4b3286453197d306f
Developer Baboo <babooheerani@gmail.com>
2024-01-10 12:14:11 +0500

    created pages

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+
+
+<template>
+  <div>
+    <h1>I am Home Page</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useHead } from "nuxt/app";
+    useHead({
+        title: 'Home Page',
+    })
+</script>
+
+
+<style>
+
+</style>
