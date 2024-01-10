14a51aafe0b4ebca06a0e125118275487001222c
Developer Baboo <babooheerani@gmail.com>
2024-01-10 12:42:21 +0500

    header and footer page

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -1,17 +1,23 @@
 <template>
   <div>
-    <h1>I am About Us Page</h1>
+    <div class="container mt-5">
+      <h1>About Us</h1>
+      <p style="justify-content:center" >We are a company that specializes in providing high-quality services. Learn more about us on this page.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eius obcaecati dignissimos praesentium placeat explicabo, aspernatur repellendus nobis alias architecto temporibus repudiandae ipsum at modi, hic quas quos beatae voluptas!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis earum eaque quaerat aut sint, ullam itaque adipisci aliquid corrupti ab laboriosam recusandae pariatur tempora autem natus. Nobis repellat recusandae modi!
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, reprehenderit enim debitis magnam suscipit tempora velit. Quos dolorem accusamus mollitia neque voluptatum impedit, atque non sit consectetur maxime nulla velit.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique iure minus explicabo beatae eligendi assumenda dolorem in repudiandae officiis at ipsum, officia quae culpa molestias laudantium voluptatem quis! Cupiditate, voluptatem.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem possimus labore voluptatibus delectus voluptate omnis vero accusantium aspernatur a nihil? Beatae iure magni aperiam a delectus repellat tempore incidunt velit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis tempore, vitae sint quidem quia deserunt, libero consequuntur atque ea in repudiandae iste dignissimos corporis odio blanditiis laudantium! Et, itaque quae.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque provident aspernatur asperiores hic quos, sunt totam culpa eius, necessitatibus molestiae neque, quidem animi rem id obcaecati facilis! Aliquid, asperiores soluta?
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis quia culpa, incidunt ipsa sapiente quibusdam ea iusto, aut, quidem hic maiores aspernatur dignissimos fugiat laborum repellat maxime eveniet aliquid! Eaque?
+      </p>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts">
-import { useHead } from "nuxt/app";
-    useHead({
-        title: 'About Us Page',
-    })
-</script>
+<script setup lang="ts"></script>
 
-
-<style>
-
+<style scoped>
+/* Add your styles here */
 </style>
--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -1,17 +1,27 @@
 <template>
   <div>
-    <h1>I am Contact Us Page</h1>
+    <div class="container mt-5">
+      <form>
+        <div class="mb-3">
+          <label for="name" class="form-label">Your Name</label>
+          <input type="text" class="form-control" id="name" placeholder="Enter your name">
+        </div>
+        <div class="mb-3">
+          <label for="email" class="form-label">Email Address</label>
+          <input type="email" class="form-control" id="email" placeholder="Enter your email">
+        </div>
+        <div class="mb-3">
+          <label for="message" class="form-label">Your Message</label>
+          <textarea class="form-control" id="message" rows="3" placeholder="Enter your message"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts">
-import { useHead } from "nuxt/app";
-    useHead({
-        title: 'Contact Us Page',
-    })
-</script>
+<script setup lang="ts"></script>
 
-
-<style>
-
+<style scoped>
+/* Add your styles here */
 </style>
